6e738c11e006f97678cec3942796ab7d967638db
IgnacioCastro0713 <ignaciomenchaca666@gmail.com>
2020-09-29 20:48:24 -0500

    folder deploy "docs"

---
+++ docs/precache-manifest.8208a7d9a19de5ac0ab146e62940be2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51873ae8170221a0a0d2291aeccc243d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "361caa20a17d08da987d",
+    "url": "/static/css/main.a493a14a.chunk.css"
+  },
+  {
+    "revision": "51581390fe19a5f5d8ce",
+    "url": "/static/js/2.83a53aea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.83a53aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "361caa20a17d08da987d",
+    "url": "/static/js/main.7c80b627.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
