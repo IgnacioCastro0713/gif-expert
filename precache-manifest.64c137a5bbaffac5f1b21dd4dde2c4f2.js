36393435b677306d3b85de641d679eeb915c2cdd
IgnacioCastro0713 <ignacio.menchaca666@gmail.com>
2020-11-14 06:09:53 +0000

    Updates

---
+++ precache-manifest.64c137a5bbaffac5f1b21dd4dde2c4f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "234ee0c72b472ba41e04421987e9bfcd",
+    "url": "/ignaciocastro0713.github.io/gif-expert/index.html"
+  },
+  {
+    "revision": "34468aef1262c9f5054b",
+    "url": "/ignaciocastro0713.github.io/gif-expert/static/css/main.8564aa7a.chunk.css"
+  },
+  {
+    "revision": "51581390fe19a5f5d8ce",
+    "url": "/ignaciocastro0713.github.io/gif-expert/static/js/2.83a53aea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ignaciocastro0713.github.io/gif-expert/static/js/2.83a53aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34468aef1262c9f5054b",
+    "url": "/ignaciocastro0713.github.io/gif-expert/static/js/main.385a7ebe.chunk.js"
+  },
+  {
+    "revision": "af54c901705032e385c6",
+    "url": "/ignaciocastro0713.github.io/gif-expert/static/js/runtime-main.387b0012.js"
+  }
+]);
