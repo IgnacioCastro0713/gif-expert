d5a01642d08145b6dc59fd56dfd9d56e28572fb0
IgnacioCastro0713 <ignaciomenchaca666@gmail.com>
2020-11-13 23:46:58 -0600

    Add simple test

--- src/index.js
+++ src/index.js
@@ -7,3 +7,7 @@ ReactDOM.render(
   <GifExpertApp/>,
   document.getElementById('root')
 );
+
+/*
+    "rename:build": "node -e \"require('fs').rename('./build', 'docs', function(err) { if (err) console.log(err); console.log('directory successfully renamed!') })\"",
+*/
